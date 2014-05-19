e8a28aeb431c158d34a6244d3ada335a8713e04e
Leo Chaisson <chaissonl@wit.edu>
2014-05-19 16:29:27 -0400

    lab 1 mock interrupts

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -2,9 +2,36 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello, World!\n");
-
+	printf("hello world");
+	nothing();
+	loopy();
 	return 0;
+}
+
+void nothing(void){
+	printf("This ISP does nothing except print this line...(why? I dot even know)");
+}
+void loopy(void){
+	int x=10;
+	while(x>1){
+		printf("x");
+		x-1;
+	}
+}
+void isp(void){
+	printf("I am mocking an interrupt!");
+}
+void thatHurt(void){
+	printf("Ouch my CPU!");
+}
+void addForMe(void){
+	int x=2;
+	x=x+x;
+	printf("4");
+}
+void theBestPokemon(void){
+	printf("My favorite Pokemon is Bidoof");
 }
